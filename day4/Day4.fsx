6e38bab3990c9f9bc0a9c8ec9d397ac263e6f20d
Julio Capote <6135+capotej@users.noreply.github.com>
2024-12-25 10:48:13 -0600

    works!

--- day4/Day4.fsx
+++ day4/Day4.fsx
@@ -9,7 +9,7 @@ let loadGridFromFile(file: string): Grid =
     let coords = dArray |> List.mapi (fun x l -> (l |> List.mapi (fun y c -> { Data = c; X = y; Y = x })))
     coords |> List.collect(fun x -> x)
 
-let grid = loadGridFromFile "day4/testinput.txt"
+let grid = loadGridFromFile "day4/input.txt"
 
 let gridContainsCoord(coord: Coordinate): bool =
     grid |> List.exists(fun x -> x = coord)
@@ -20,44 +20,51 @@ let gridContainsWord(w: Word): bool =
 let candidates(c: Coordinate): Word list =
     let y, x = c.Y, c.X
 
-    [ [ { Y = y - 1; X = x; Data = 'M' }
+    [ [ { Y = y; X = x; Data = 'X'} 
+        { Y = y - 1; X = x; Data = 'M' }
         { Y = y - 2; X = x; Data = 'A' }
         { Y = y - 3; X = x; Data = 'S' } ] |> List.toSeq // 1
-      [ { Y = y; X = x - 1; Data = 'M' }
+
+      [ { Y = y; X = x; Data = 'X'}
+        { Y = y; X = x - 1; Data = 'M' }
         { Y = y; X = x - 2; Data = 'A' }
         { Y = y; X = x - 3; Data = 'S' } ] |> List.toSeq // 2
-      [ { Y = y - 1; X = x - 1; Data = 'M' }
+
+      [ { Y = y; X = x; Data = 'X'}
+        { Y = y - 1; X = x - 1; Data = 'M' }
         { Y = y - 2; X = x - 2; Data = 'A' }
         { Y = y - 3; X = x - 3; Data = 'S' } ] |> List.toSeq // 3
-      [ { Y = y + 1; X = x; Data = 'M' }
+
+      [ { Y = y; X = x; Data = 'X'}
+        { Y = y + 1; X = x; Data = 'M' }
         { Y = y + 2; X = x; Data = 'A' }
         { Y = y + 3; X = x; Data = 'S' } ] |> List.toSeq // 4
-      [ { Y = y; X = x + 1; Data = 'M' }
+
+      [ { Y = y; X = x; Data = 'X'}
+        { Y = y; X = x + 1; Data = 'M' }
         { Y = y; X = x + 2; Data = 'A' }
         { Y = y; X = x + 3; Data = 'S' } ] |> List.toSeq // 5
-      [ { Y = y + 1; X = x + 1; Data = 'M' }
+
+      [ { Y = y; X = x; Data = 'X'}
+        { Y = y + 1; X = x + 1; Data = 'M' }
         { Y = y + 2; X = x + 2; Data = 'A' }
         { Y = y + 3; X = x + 3; Data = 'S' } ] |> List.toSeq // 6
-      [ { Y = y + 1; X = x - 1; Data = 'M' }
+
+      [ { Y = y; X = x; Data = 'X'}
+        { Y = y + 1; X = x - 1; Data = 'M' }
         { Y = y + 2; X = x - 2; Data = 'A' }
-        { Y = x + 3; X = x - 3; Data = 'S' } ] |> List.toSeq // 7
-      [ { Y = y - 1; X = x + 1; Data = 'M' }
+        { Y = y + 3; X = x - 3; Data = 'S' } ] |> List.toSeq // 7
+
+      [ { Y = y; X = x; Data = 'X'}
+        { Y = y - 1; X = x + 1; Data = 'M' }
         { Y = y - 2; X = x + 2; Data = 'A' }
         { Y = y - 3; X = x + 3; Data = 'S' } ] |> List.toSeq // 8 
     ] 
 
 let wordsFoundForCoord(c: Coordinate): int =
-    // search all directions
     (0, (candidates c)) ||> List.fold(fun s w -> if gridContainsWord w then s + 1 else s)
 
 let result =
     grid
-    |> List.filter (fun x -> x.Data = 'X')
     |> List.map wordsFoundForCoord
     |> List.sum
-
-
-
-
-
-
