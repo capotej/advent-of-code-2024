916cb3a3275e37575dc5271469703bbde938b6c3
Julio Capote <6135+capotej@users.noreply.github.com>
2024-12-25 10:35:22 -0600

    off by one lol

--- day4/Day4.fsx
+++ day4/Day4.fsx
@@ -1,51 +1,63 @@
-let lines =
-    [ for line in System.IO.File.ReadLines("day4/input.txt") -> line]
+type Coordinate = {X:int; Y:int; Data:char}
+type Grid = Coordinate list
+type Word = Coordinate seq
 
-// note: this is y,x format
-let grid = lines |> List.map(fun x -> x |> Seq.toList)
+let loadGridFromFile(file: string): Grid = 
+    let lines = [ for line in System.IO.File.ReadLines(file) -> line]
+    let dArray = lines |> List.map(fun x -> x |> Seq.toList)
+    // note: this is y,x format
+    let coords = dArray |> List.mapi (fun x l -> (l |> List.mapi (fun y c -> { Data = c; X = y; Y = x })))
+    coords |> List.collect(fun x -> x)
 
-// handles bounds
-let lookup(y: int)(x: int): char =
-    let y = if y < 0 then 0 else y
-    let x = if x < 0 then 0 else x
-    let y = if y > 139 then 139 else y
-    let x = if x > 139 then 139 else x
-    grid[y][x]
+let grid = loadGridFromFile "day4/testinput.txt"
 
-let all = List.allPairs [0..139] [0..139]
+let gridContainsCoord(coord: Coordinate): bool =
+    grid |> List.exists(fun x -> x = coord)
 
-let starters =
-    all
-    |> List.filter (fun (pair: int * int) ->
-        let y, x = pair
-        (grid[y][x]).Equals 'X')
+let gridContainsWord(w: Word): bool = 
+    w |> Seq.forall(fun x -> gridContainsCoord x)
 
-// we need a function that returns the list of `(int * int) list` of c found when found, rather than bool
-// let adjacentChar (c: char) (pair: int * int): (int * int) = 
-    // let y, x = pair
-    // (lookup (y - 1) (x)).Equals(c) // 1
-    // (lookup (y) (x - 1)).Equals(c) || // 2
-    // (lookup (y - 1) (x - 1)).Equals(c) || // 3
-    // (lookup (y + 1) (x)).Equals(c) || // 4
-    // (lookup (y) (x + 1)).Equals(c) || // 5
-    // (lookup (y + 1) (x + 1)).Equals(c) || // 6
-    // (lookup (y + 1) (x - 1)).Equals(c) || // 7
-    // (lookup (y - 1) (x + 1)).Equals(c)  // 8
+let candidates(c: Coordinate): Word list =
+    let y, x = c.Y, c.X
+
+    [ [ { Y = y - 1; X = x; Data = 'M' }
+        { Y = y - 2; X = x; Data = 'A' }
+        { Y = y - 3; X = x; Data = 'S' } ] |> List.toSeq // 1
+      [ { Y = y; X = x - 1; Data = 'M' }
+        { Y = y; X = x - 2; Data = 'A' }
+        { Y = y; X = x - 3; Data = 'S' } ] |> List.toSeq // 2
+      [ { Y = y - 1; X = x - 1; Data = 'M' }
+        { Y = y - 2; X = x - 2; Data = 'A' }
+        { Y = y - 3; X = x - 3; Data = 'S' } ] |> List.toSeq // 3
+      [ { Y = y + 1; X = x; Data = 'M' }
+        { Y = y + 2; X = x; Data = 'A' }
+        { Y = y + 3; X = x; Data = 'S' } ] |> List.toSeq // 4
+      [ { Y = y; X = x + 1; Data = 'M' }
+        { Y = y; X = x + 2; Data = 'A' }
+        { Y = y; X = x + 3; Data = 'S' } ] |> List.toSeq // 5
+      [ { Y = y + 1; X = x + 1; Data = 'M' }
+        { Y = y + 2; X = x + 2; Data = 'A' }
+        { Y = y + 3; X = x + 3; Data = 'S' } ] |> List.toSeq // 6
+      [ { Y = y + 1; X = x - 1; Data = 'M' }
+        { Y = y + 2; X = x - 2; Data = 'A' }
+        { Y = x + 3; X = x - 3; Data = 'S' } ] |> List.toSeq // 7
+      [ { Y = y - 1; X = x + 1; Data = 'M' }
+        { Y = y - 2; X = x + 2; Data = 'A' }
+        { Y = y - 3; X = x + 3; Data = 'S' } ] |> List.toSeq // 8 
+    ] 
+
+let wordsFoundForCoord(c: Coordinate): int =
+    // search all directions
+    (0, (candidates c)) ||> List.fold(fun s w -> if gridContainsWord w then s + 1 else s)
+
+let result =
+    grid
+    |> List.filter (fun x -> x.Data = 'X')
+    |> List.map wordsFoundForCoord
+    |> List.sum
 
 
-let hasAdjacentChar (c: char) (pair: int * int): bool = 
-    let y, x = pair
-    (lookup (y - 1) (x)).Equals(c) || // 1
-    (lookup (y) (x - 1)).Equals(c) || // 2
-    (lookup (y - 1) (x - 1)).Equals(c) || // 3
-    (lookup (y + 1) (x)).Equals(c) || // 4
-    (lookup (y) (x + 1)).Equals(c) || // 5
-    (lookup (y + 1) (x + 1)).Equals(c) || // 6
-    (lookup (y + 1) (x - 1)).Equals(c) || // 7
-    (lookup (y - 1) (x + 1)).Equals(c)  // 8
 
 
-starters |> List.filter(hasAdjacentChar 'M')
-         |> List.filter(hasAdjacentChar 'A') // starters is still the 'X', we need to filter on M coords...
-         |> List.filter(hasAdjacentChar 'S')
-         |> List.length
+
+
